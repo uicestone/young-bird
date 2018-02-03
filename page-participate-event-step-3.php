fd7f7934fa423cc30163e53b529e5b5771dbafa6
Uice Lu <uicestone@gmail.com>
2018-02-03 17:52:56 +0800

    批量替换footer

--- page-participate-event-step-3.php
+++ page-participate-event-step-3.php
@@ -28,35 +28,4 @@
         </div>
       </div>
     </div>
-    <!-- Footer -->
-    <div class="footer">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-8">
-            <ul class="list-inline">
-              <li class="list-inline-item mr-4">
-                <a href="#">关于我们</a>
-              </li>
-              <li class="list-inline-item mr-4">
-                <a href="#">商务合作</a>
-              </li>
-              <li class="list-inline-item mr-4">
-                <a href="#">帮助中心</a>
-              </li>
-            </ul>
-          </div>
-          <div class="col-lg-16">
-            <span class="mr-md-4">TELEPHONE & FAX&nbsp;&nbsp;+86 021 6258 5617&nbsp;&nbsp;+86 021 6258 5617-102</span>
-            <span class="nowrap">E-MAIL&nbsp;&nbsp;competition@youngbird.com.cn</span>
-            <span class="nowrap">沪ICP备 16055号-1|© 2016-2017 Design Verse 版权所有</span>
-          </div>
-        </div>
-
-      </div>
-    </div>
-
-    <script type="text/javascript" src="./bootstrap/js/jquery-3.2.1.slim.min.js"></script>
-    <script type="text/javascript" src="./bootstrap/js/popper.min.js"></script>
-    <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
-  </body>
-</html>
+<?php get_footer(); ?>
--- single-judge.php
+++ single-judge.php
@@ -19,35 +19,4 @@
         <p>琚宾作品：阳朔Alila酒店致力于研究中国文化在建筑空间里的运用和创新，以个性化、独特的视觉语言来表达设计理念，以全新的视觉传达来解读中国文化元素。在作品中，将“当代性”、“文化性”、“艺术性”共溶、共生，以此作为设计语言用于空间表达。从传统与当下的共通、碰撞处，找寻设计的灵感；在艺术与生活的交错、和谐处，追求设计的本质。在历史的记忆碎片与当下思想的结合中，寻找设计文化的精神诉求。</p>
       </div>
     </div>
-    <!-- Footer -->
-    <div class="footer">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-8">
-            <ul class="list-inline">
-              <li class="list-inline-item mr-4">
-                <a href="#">关于我们</a>
-              </li>
-              <li class="list-inline-item mr-4">
-                <a href="#">商务合作</a>
-              </li>
-              <li class="list-inline-item mr-4">
-                <a href="#">帮助中心</a>
-              </li>
-            </ul>
-          </div>
-          <div class="col-lg-16">
-            <span class="mr-md-4">TELEPHONE & FAX&nbsp;&nbsp;+86 021 6258 5617&nbsp;&nbsp;+86 021 6258 5617-102</span>
-            <span class="nowrap">E-MAIL&nbsp;&nbsp;competition@youngbird.com.cn</span>
-            <span class="nowrap">沪ICP备 16055号-1|© 2016-2017 Design Verse 版权所有</span>
-          </div>
-        </div>
-
-      </div>
-    </div>
-
-    <script type="text/javascript" src="./bootstrap/js/jquery-3.2.1.slim.min.js"></script>
-    <script type="text/javascript" src="./bootstrap/js/popper.min.js"></script>
-    <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
-  </body>
-</html>
+<?php get_footer(); ?>
