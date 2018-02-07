c8f86ec7694e37b067b1f881dcfcc99a3d108b67
Uice Lu <uicestone@gmail.com>
2018-02-07 20:37:08 +0800

    用户名支持中文

--- functions.php
+++ functions.php
@@ -159,3 +159,23 @@ add_action('wp_enqueue_scripts', function(){
   wp_enqueue_script('popper');
   wp_enqueue_script('main');
 });
+
+add_filter ('sanitize_user', function ($username, $raw_username, $strict) {
+  $username = wp_strip_all_tags( $raw_username );
+  $username = remove_accents( $username );
+  // Kill octets
+  $username = preg_replace( '|%([a-fA-F0-9][a-fA-F0-9])|', '', $username );
+  $username = preg_replace( '/&.+?;/', '', $username ); // Kill entities
+
+  // 网上很多教程都是直接将$strict赋值false，
+  // 这样会绕过字符串检查，留下隐患
+  if ($strict) {
+    $username = preg_replace ('|[^a-z\p{Han}0-9 _.\-@]|iu', '', $username);
+  }
+
+  $username = trim( $username );
+  // Consolidate contiguous whitespace
+  $username = preg_replace( '|\s+|', ' ', $username );
+
+  return $username;
+}, 10, 3);
