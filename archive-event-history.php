9fadc757833b68cbdf55fe9d4ab72eca8e7c6cb5
rainkolwa <pluvo.lee@gmail.com>
2018-02-09 18:34:07 +0800

    历史列表样式错位

--- archive-event-history.php
+++ archive-event-history.php
@@ -12,25 +12,28 @@
             <?php while (have_posts()): the_post(); ?>
             <div class="col-md-12">
               <div class="card mb-4 item-history">
-                <a href="<?php the_permalink(); ?>"><img class="card-img-top" src="<?=get_stylesheet_directory_uri()?>/images/sample/poster-history.jpg" alt="Card image cap">
-                <div class="card-body mt-4">
-                  <div class="row head justify-content-between align-items-center">
-                    <div class="label color-rose">Pattern design</div>
-                    <div>2017/01/01-2017/6/20</div>
+                <a href="<?php the_permalink(); ?>">
+                  <img class="card-img-top" src="<?=get_stylesheet_directory_uri()?>/images/sample/poster-history.jpg" alt="Card image cap">
+                  <div class="card-body mt-4">
+                    <div class="row head justify-content-between align-items-center">
+                      <div class="label color-rose">Pattern design</div>
+                      <div>2017/01/01-2017/6/20</div>
+                    </div>
+                    <h3 class="mt-3">2017 良辰美景<br>遮阳伞面设计竞赛</h3>
+                    <p class="color-black mb-4">竞赛品牌协作方／sunbrella赛百伦</p>
+                    <p>户外遮阳伞在城市公共场所中的使用随处可见。它不仅成为人们抵抗紫外线的一道屏障，也装点着城市的户外环境。然而国内市场上的户外遮阳伞普遍存在着褪色，发霉，肮脏和破损等现象。</p>
+                    <div class="action row align-items-center">
+                      <i class="far fa-user mr-2"></i>
+                      <span class="mr-4">参赛人数 / 921</span>
+                      <i class="far fa-heart"></i>
+                    </div>
                   </div>
-                  <h3 class="mt-3">2017 良辰美景<br>遮阳伞面设计竞赛</h3>
-                  <p class="color-black mb-4">竞赛品牌协作方／sunbrella赛百伦</p>
-                  <p>户外遮阳伞在城市公共场所中的使用随处可见。它不仅成为人们抵抗紫外线的一道屏障，也装点着城市的户外环境。然而国内市场上的户外遮阳伞普遍存在着褪色，发霉，肮脏和破损等现象。</p>
-                  <div class="action row align-items-center">
-                    <i class="far fa-user mr-2"></i>
-                    <span class="mr-4">参赛人数 / 921</span>
-                    <i class="far fa-heart"></i>
-                  </div>
-                </div>
+                </a>
               </div>
             </div>
             <?php endwhile; ?>
-          <button type="button" class="btn btn-outline-secondary mx-auto d-block btn-common mb-4">发现更多</button>
+            <button type="button" class="btn btn-outline-secondary mx-auto d-block btn-common mb-4">发现更多</button>
+          </div>
         </div>
         <div class="col-md-6">
           <div class="card mb-3 item-sub-history">
