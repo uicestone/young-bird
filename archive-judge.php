e8fc50c0d0aaa9776c90db2538e1cb00afccd789
rainkolwa <pluvo.lee@gmail.com>
2018-02-09 18:53:07 +0800

    一些anchor

--- archive-judge.php
+++ archive-judge.php
@@ -8,7 +8,7 @@
     <!-- Body -->
     <div class="container mt-7 pb-4">
       <div class="row">
-        <div class="col-sm-12 col-md-8 col-lg-6 mb-5">
+        <a href="#" class="col-sm-12 col-md-8 col-lg-6 mb-5 link">
           <div class="card item-judge">
             <img class="card-img-top" src="<?=get_stylesheet_directory_uri()?>/images/sample/judge.jpg" alt="Card image cap">
             <div class="card-body mt-4">
@@ -16,8 +16,8 @@
               <p>著名美籍华人产品设计师<br>著名美籍美籍美籍华人产品设计师</p>
             </div>
           </div>
-        </div>
-        <div class="col-sm-12 col-md-8 col-lg-6 mb-5">
+        </a>
+        <a href="#" class="col-sm-12 col-md-8 col-lg-6 mb-5 link">
           <div class="card item-judge">
             <img class="card-img-top" src="<?=get_stylesheet_directory_uri()?>/images/sample/judge.jpg" alt="Card image cap">
             <div class="card-body mt-4">
@@ -25,43 +25,7 @@
               <p>著名美籍华人产品设计师<br>著名美籍美籍美籍华人产品设计师</p>
             </div>
           </div>
-        </div>
-        <div class="col-sm-12 col-md-8 col-lg-6 mb-5">
-          <div class="card item-judge">
-            <img class="card-img-top" src="<?=get_stylesheet_directory_uri()?>/images/sample/judge.jpg" alt="Card image cap">
-            <div class="card-body mt-4">
-              <h5>是大雨</h5>
-              <p>著名美籍华人产品设计师<br>著名美籍美籍美籍华人产品设计师</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-12 col-md-8 col-lg-6 mb-5">
-          <div class="card item-judge">
-            <img class="card-img-top" src="<?=get_stylesheet_directory_uri()?>/images/sample/judge.jpg" alt="Card image cap">
-            <div class="card-body mt-4">
-              <h5>是大雨</h5>
-              <p>著名美籍华人产品设计师<br>著名美籍美籍美籍华人产品设计师</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-12 col-md-8 col-lg-6 mb-5">
-          <div class="card item-judge">
-            <img class="card-img-top" src="<?=get_stylesheet_directory_uri()?>/images/sample/judge.jpg" alt="Card image cap">
-            <div class="card-body mt-4">
-              <h5>是大雨</h5>
-              <p>著名美籍华人产品设计师<br>著名美籍美籍美籍华人产品设计师</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-12 col-md-8 col-lg-6 mb-5">
-          <div class="card item-judge">
-            <img class="card-img-top" src="<?=get_stylesheet_directory_uri()?>/images/sample/judge.jpg" alt="Card image cap">
-            <div class="card-body mt-4">
-              <h5>是大雨</h5>
-              <p>著名美籍华人产品设计师<br>著名美籍美籍美籍华人产品设计师</p>
-            </div>
-          </div>
-        </div>
+        </a>
       </div>
     </div>
 <?php get_footer(); ?>
