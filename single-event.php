da46429bb02eebd24b257098b4d5a67dd7560189
rainkolwa <pluvo.lee@gmail.com>
2018-02-11 21:09:34 +0800

    竞赛详情页面

---
+++ single-event.php
@@ -0,0 +1,35 @@
+<?php get_header(); ?>
+    <!-- Body -->
+    <div class="container pb-4">
+      <div class="sidebar">
+        <ul>
+          <li>
+            <a href="#">竞赛命题</a>
+          </li>
+          <li>
+            <a href="#">奖项设置</a>
+          </li>
+          <li>
+            <a href="#">评委介绍</a>
+          </li>
+          <li>
+            <a href="#">Q&A</a>
+          </li>
+          <li>
+            <a href="#">相关新闻</a>
+          </li>
+          <li>
+            <a href="#">下载文件</a>
+          </li>
+          <li>
+            <a href="#">参赛</a>
+          </li>
+        </ul>
+      </div>
+      <div class="content">
+        <div class="poster">
+          <img src="<?=get_stylesheet_directory_uri()?>/images/sample/event-detail-poster.jpg" alt="">
+        </div>
+      </div>
+    </div>
+<?php get_footer(); ?>
