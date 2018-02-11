c7779f0380b48d8c7544c1a1baf0b6d2140ab565
Uice Lu <uicestone@gmail.com>
2018-02-11 18:51:02 +0800

    新闻详情页动态化

--- single.php
+++ single.php
@@ -1,20 +1,25 @@
-<?php get_header(); ?>
+<?php
+$views = (get_post_meta(get_the_ID(), 'views', true) ?: 0) + 1;
+update_post_meta(get_the_ID(), 'views', $views);
+$likes = get_post_meta(get_the_ID(), 'likes', true) ?: 0;
+
+get_header(); the_post(); ?>
     <!-- Body -->
     <div class="container pb-4">
       <div class="row">
         <div class="col-md-18 news-detail">
           <section class="header">
             <!-- poster -->
-            <img src="<?=get_stylesheet_directory_uri()?>/images/sample/poster-news-detail.jpg" alt="" class="mb-4" width="100%">
+            <?php the_post_thumbnail('5-3', array('class'=>'mb-4')) ?>
             <!-- title -->
-            <h1 class="color-black">深圳蛇口太子湾公共文化建筑设计竞赛</h1>
-            <span>发布时间 2017-12-12</span>
+            <h1 class="color-black"><?php the_title(); ?></h1>
+            <span>发布时间 <?php the_date(); ?></span>
             <div class="row justify-content-between align-items-center mt-4">
               <div class="d-flex align-items-center">
                 <i class="fas fa-eye mr-2"></i>
-                <span class="mr-4">921</span>
+                <span class="mr-4"><?=$views?></span>
                 <i class="far fa-heart mr-2"></i>
-                <span class="mr-4">21</span>
+                <span class="mr-4"><?=$likes?></span>
               </div>
               <div>
                 分享至：
@@ -22,41 +27,31 @@
             </div>
           </section>
           <div class="editor my-5">
-            <p>深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛。</p>
-            <p>深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛。</p>
-            <p>深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛。</p><p>深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛。</p><p>深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛。</p><p>深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛。</p><p>深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛深圳蛇口太子湾公共文化建筑设计竞赛，深圳蛇口太子湾公共文化建筑设计竞赛。</p>
+            <?php the_content(); ?>
           </div>
-          <button type="button" class="btn btn-outline-secondary mx-auto d-block btn-common mb-4">立即报名</button>
+          <?php if ($event = get_field('event')): ?>
+          <a href="<?=get_the_permalink($event->ID)?>" class="btn btn-outline-secondary mx-auto d-block btn-common mb-4">立即报名</a>
+          <?php endif; ?>
         </div>
         <div class="col-md-6">
-          <a href="#" class="card mb-3 item-sub-history">
-            <img class="card-img-top" src="<?=get_stylesheet_directory_uri()?>/images/sample/poster-history.jpg" alt="Card image cap">
+          <?php foreach (get_posts(array ('category_name' => 'ad')) as $ad): ?>
+          <a href="<?=get_the_permalink($ad->ID)?>" class="card mb-3 item-sub-history">
+            <?=get_the_post_thumbnail($ad->ID, '8-7', array ('class' => 'card-img-top'))?>
             <div class="card-label">
-              <span>#建筑设计</span>
+              <?php foreach (get_the_tags($ad->ID) as $tag): ?>
+              <span>#<?=$tag->name?></span>
+              <?php endforeach; ?>
               <div class="tag tag-blue">工作</div>
             </div>
             <hr />
             <div class="card-body">
-              <h4>2015 IN-BETWEEN<br>深圳蛇口太子湾公共文化建筑设计竞赛</h4>
-              <p>
-                截止日期：2017年11月17日15：00
-              </p>
+              <h4><?=get_the_title($ad->ID)?><br><?=get_the_subtitle($ad->ID)?></h4>
+              <?php if ($ad_event = get_field('event', $ad)): ?>
+              <p>截止日期：<?=get_post_meta($ad_event->ID, 'end_date', true)?></p>
+              <?php endif; ?>
             </div>
           </a>
-          <a href="#" class="card mb-3 item-sub-history">
-            <img class="card-img-top" src="<?=get_stylesheet_directory_uri()?>/images/sample/poster-history.jpg" alt="Card image cap">
-            <div class="card-label">
-              <span>#建筑设计</span>
-              <div class="tag tag-green">新闻</div>
-            </div>
-            <hr />
-            <div class="card-body">
-              <h4>2015 IN-BETWEEN<br>深圳蛇口太子湾公共文化建筑设计竞赛</h4>
-              <p>
-                截止日期：2017年11月17日15：00
-              </p>
-            </div>
-          </a>
+          <?php endforeach; ?>
         </div>
       </div>
     </div>
