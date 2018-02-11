c02c6070a5b695dcfbdbac028902fce7c4ad250c
Uice Lu <uicestone@gmail.com>
2018-02-11 19:03:42 +0800

    竞赛列表动态化

--- archive-event.php
+++ archive-event.php
@@ -54,18 +54,18 @@
             <div class="col-md-12">
               <div class="card mb-4 item-history">
                 <a href="<?php the_permalink(); ?>">
-                  <img class="card-img-top" src="<?=get_stylesheet_directory_uri()?>/images/sample/poster-history.jpg" alt="Card image cap">
+                  <?php the_post_thumbnail('vga', array ('class' => 'card-img-top')); ?>
                   <div class="card-body mt-4">
                     <div class="row head justify-content-between align-items-center">
                       <div class="label color-rose">Pattern design</div>
-                      <div>2017/01/01-2017/6/20</div>
+                      <div><?=get_post_meta(get_the_ID(), 'start_date', true)?> ~ <?=get_post_meta(get_the_ID(), 'end_date', true)?></div>
                     </div>
-                    <h3 class="mt-3">2017 良辰美景<br>遮阳伞面设计竞赛</h3>
-                    <p class="color-black mb-4">竞赛品牌协作方／sunbrella赛百伦</p>
-                    <p>户外遮阳伞在城市公共场所中的使用随处可见。它不仅成为人们抵抗紫外线的一道屏障，也装点着城市的户外环境。然而国内市场上的户外遮阳伞普遍存在着褪色，发霉，肮脏和破损等现象。</p>
+                    <h3 class="mt-3"><?php the_title(); ?><br><?php the_subtitle(); ?></h3>
+                    <p class="color-black mb-4">竞赛品牌协作方／<?=get_post_meta(get_the_ID(), 'organizer', true)?></p>
+                    <p><?php the_excerpt(); ?></p>
                     <div class="action row align-items-center">
                       <i class="far fa-user mr-2"></i>
-                      <span class="mr-4">参赛人数 / 921</span>
+                      <span class="mr-4">参赛人数 / <?=get_post_meta(get_the_ID(), 'attendees', true)?></span>
                       <i class="far fa-heart"></i>
                     </div>
                   </div>
@@ -77,24 +77,17 @@
           <button type="button" class="btn btn-outline-secondary mx-auto d-block btn-common mb-4">发现更多</button>
         </div>
         <div class="col-md-6">
+          <?php foreach (get_posts(array ('category_name' => 'ad')) as $ad): ?>
           <a href="#" class="card mb-3 item-sub-history">
-            <img class="card-img-top" src="<?=get_stylesheet_directory_uri()?>/images/sample/poster-history.jpg" alt="Card image cap">
+            <?=get_the_post_thumbnail($ad->ID, '8-7', array ('class' => 'card-img-top'))?>
             <div class="card-body">
-              <h4>2015 IN-BETWEEN<br>深圳蛇口太子湾公共文化建筑设计竞赛</h4>
-              <p>
-                截止日期：2017年11月17日15：00
-              </p>
+              <h4><?=get_the_title($ad->ID)?><br><?=get_the_subtitle($ad->ID)?></h4>
+              <?php if ($ad_event = get_field('event', $ad)): ?>
+                <p>截止日期：<?=get_post_meta($ad_event->ID, 'end_date', true)?></p>
+              <?php endif; ?>
             </div>
           </a>
-          <a href="#" class="card mb-3 item-sub-history">
-            <img class="card-img-top" src="<?=get_stylesheet_directory_uri()?>/images/sample/poster-history.jpg" alt="Card image cap">
-            <div class="card-body">
-              <h4>2015 IN-BETWEEN<br>深圳蛇口太子湾公共文化建筑设计竞赛</h4>
-              <p>
-                截止日期：2017年11月17日15：00
-              </p>
-            </div>
-          </a>
+          <?php endforeach; ?>
         </div>
       </div>
     </div>
