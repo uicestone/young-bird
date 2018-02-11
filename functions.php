f4ebb3850d0c54e08903bef23ccf51d0316baefe
Uice Lu <uicestone@gmail.com>
2018-02-11 13:20:34 +0800

    WP_DEVELOP模式下替换upload_url为线上

--- functions.php
+++ functions.php
@@ -198,3 +198,7 @@ add_filter ('sanitize_user', function ($username, $raw_username, $strict) {
 
   return $username;
 }, 10, 3);
+
+if (WP_DEVELOP) {
+  add_filter( 'pre_option_upload_url_path', function() { return 'http://www.youngbirdplan.com.cn/wp-content/uploads'; } );
+}
