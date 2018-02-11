050f5959376fd425ce55c3ddfb4dbaba7ca10265
Uice Lu <uicestone@gmail.com>
2018-02-11 19:23:52 +0800

    大咖详情动态化

--- single-judge.php
+++ single-judge.php
@@ -1,22 +1,22 @@
-<?php get_header(); ?>
+<?php get_header(); the_post(); ?>
     <!-- Banner -->
     <div class="container-fluid judge-detail" style="padding: 0;background: #c8c8c8">
       <div class="container banner d-flex py-4">
         <div class="avatar">
-          <img src="<?=get_stylesheet_directory_uri()?>/images/sample/judge.jpg" width="100%" alt="">
+          <?php the_post_thumbnail('medium-sq'); ?>
         </div>
         <div class="desc pl-3 pl-lg-5">
-          <h1 class="color-black font-weight-bold">鲁求是</h1>
-          <h3 class="color-black font-weight-bold">设计师</h3>
-          <h3 class="color-black font-weight-bold">撒的接口理发师 && 啊手机号撒上</h3>
-          <p class="mb-0">阿斯顿狂欢节富士康 v 个收到回复 v 感觉，爱上法国 v 阿福讲噶是 v讲啊</p>
+          <h1 class="color-black font-weight-bold"><?php the_title(); ?></h1>
+          <h3 class="color-black font-weight-bold">资料1</h3>
+          <h3 class="color-black font-weight-bold">资料2</h3>
+          <p class="mb-0"><?php the_excerpt(); ?></p>
         </div>
       </div>
     </div>
     <!-- Body -->
     <div class="container mt-6 pb-7">
       <div class="editor">
-        <p>琚宾作品：阳朔Alila酒店致力于研究中国文化在建筑空间里的运用和创新，以个性化、独特的视觉语言来表达设计理念，以全新的视觉传达来解读中国文化元素。在作品中，将“当代性”、“文化性”、“艺术性”共溶、共生，以此作为设计语言用于空间表达。从传统与当下的共通、碰撞处，找寻设计的灵感；在艺术与生活的交错、和谐处，追求设计的本质。在历史的记忆碎片与当下思想的结合中，寻找设计文化的精神诉求。</p>
+        <?php the_content(); ?>
       </div>
     </div>
 <?php get_footer(); ?>
