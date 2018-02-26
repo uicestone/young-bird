1641d6eb8e2ef883df6989d4394be87794536e30
rainkolwa <pluvo.lee@gmail.com>
2018-02-26 20:46:36 +0800

    Top50适配

--- single-rank-more.php
+++ single-rank-more.php
@@ -16,7 +16,7 @@
     <div class="container mt-5 pb-6 toplist-container">
       <h1 class="text-center color-dark-yellow">TOP50</h1>
       <div class="row mt-5">
-        <div class="col-sm-12 col-md-8 col-lg-2-4 mb-4">
+        <div class="col-sm-12 col-md-6 col-lg-2-4 mb-2 mb-md-4">
           <div class="card mb-4 item-history item-top50 item-work">
             <img class="card-img-top" src="<?=get_stylesheet_directory_uri()?>/images/sample/poster-history.jpg" alt="Card image cap">
             <div class="card-body mt-4">
@@ -52,7 +52,7 @@
             </a>
           </div>
         </div>
-        <div class="col-sm-12 col-md-8 col-lg-2-4 mb-4">
+        <div class="col-sm-12 col-md-6 col-lg-2-4 mb-2 mb-md-4">
           <div class="card mb-4 item-history item-top50 item-work">
             <img class="card-img-top" src="<?=get_stylesheet_directory_uri()?>/images/sample/poster-history.jpg" alt="Card image cap">
             <div class="card-body mt-4">
@@ -60,7 +60,7 @@
                 <div class="label color-black font-weight-bold">彻骨分阿斯</div>
                 <div>YB20114</div>
               </div>
-              <p class="pt-3">户外遮阳伞在城市公共场所中的使用随处可见。</p>
+              <p class="pt-3">户外遮阳伞在城市公共场所中的使用随处可见。它不仅成为人们抵抗紫外线的一道屏障，也装点着城市的户外环境。然而国内市场上的户外遮阳伞普遍存在着褪色，发霉，肮脏和破损等现象。</p>
               <div class="action row align-items-center">
                 <i class="fas fa-eye mr-1"></i>
                 <span class="mr-2">921</span>
