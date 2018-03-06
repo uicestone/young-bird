6cc0a83600dd2d958b893c4e2fcc5a53b71063a3
Uice Lu <uicestone@gmail.com>
2018-03-06 12:27:06 +0800

    fix: 语言切换按钮不显示

--- header.php
+++ header.php
@@ -43,7 +43,7 @@
             <span>|</span>
             <a href="<?=pll_home_url()?>sign-in/" class="btn btn-link"><?=__('登录', 'young-bird')?></a>
             <?php endif; ?>
-            <?php if (function_exists('pll_the_language')): ?>
+            <?php if (function_exists('pll_the_languages')): ?>
             <ul class="language-swicher mb-0">
               <?php pll_the_languages(array ('hide_current' => true, 'display_names_as' => 'slug')); ?>
             </ul>
