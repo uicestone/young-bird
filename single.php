a08e328964d1d36f2aee9e8597d20770fb4b302f
Uice Lu <uicestone@gmail.com>
2018-06-05 14:14:55 +0800

    英文简历投递流程调试

--- single.php
+++ single.php
@@ -73,15 +73,15 @@ get_header(); the_post(); ?>
           <?php
             endif;
           endif; ?>
-          <?php if (get_field('attendable')): if (in_array($id_dl, get_user_meta(get_current_user_id(), 'attend_activities') ?: array())): ?>
+          <?php if (get_field('attendable', $id_dl)): if (in_array($id_dl, get_user_meta(get_current_user_id(), 'attend_activities') ?: array())): ?>
           <button type="button" disabled class="btn btn-outline-primary mx-auto d-block btn-common mb-4 attend-activity"><?=__('已报名', 'young-bird')?></button>
           <?php else: ?>
-          <a href="<?=site_url()?>/user-center/?attend-activity=<?=$id_dl?>" class="btn btn-outline-primary mx-auto d-block btn-common mb-4 attend-activity"><?=__('立即报名', 'young-bird')?></a>
+          <a href="<?=get_permalink(get_page_by_path('user-center')->ID)?>?attend-activity=<?=$id_dl?>" class="btn btn-outline-primary mx-auto d-block btn-common mb-4 attend-activity"><?=__('立即报名', 'young-bird')?></a>
           <?php endif; endif; ?>
-          <?php if (get_field('recruitment')): if (in_array($id_dl, get_user_meta(get_current_user_id(), 'apply_jobs') ?: array())): ?>
+          <?php if (get_field('recruitment', $id_dl)): if (in_array($id_dl, get_user_meta(get_current_user_id(), 'apply_jobs') ?: array())): ?>
           <button type="button" disabled class="btn btn-outline-primary mx-auto d-block btn-common mb-4 attend-activity"><?=__('已投递简历', 'young-bird')?></button>
           <?php else: ?>
-          <a href="<?=site_url()?>/user-center/?recruitment=<?=$id_dl?>" class="btn btn-outline-primary mx-auto d-block btn-common mb-4 apply-joy"><?=__('投递简历', 'young-bird')?></a>
+          <a href="<?=get_permalink(get_page_by_path('user-center')->ID)?>?recruitment=<?=$id_dl?>" class="btn btn-outline-primary mx-auto d-block btn-common mb-4 apply-joy"><?=__('投递简历', 'young-bird')?></a>
           <?php endif; endif; ?>
         </div>
         <div class="col-md-6 news-detail-ad">
