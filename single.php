a3104a6fbdf360556fe7218725a36fcd9edee700
Uice Lu <uicestone@gmail.com>
2018-06-06 23:00:30 +0800

    文章报名按钮等置于文章首尾

--- single.php
+++ single.php
@@ -50,37 +50,11 @@ get_header(); the_post(); ?>
               </div>
             </div>
           </section>
+          <?php include(locate_template('single-action-button.php')); ?>
           <div class="editor my-3 my-md-5">
             <?php the_content(); ?>
           </div>
-          <?php
-          $event_id = get_post_meta($id_dl, 'event', true);
-          if ($event_id
-            && in_array(get_post_meta($event_id, 'status', true), array('started', 'ending'))
-            && !in_array($event_id, get_user_meta(get_current_user_id(), 'attend_events') ?: array())
-          ):
-            if (get_field('attend_event_review', $id_dl)):
-              if (!in_array($id_dl, get_user_meta(get_current_user_id(), 'attend_event_review') ?: array())): ?>
-            <a href="<?=get_permalink(get_page_by_path('user-center')->ID)?>?attend-event-review=<?=$id_dl?>" class="attend-event-review btn btn-outline-primary mx-auto d-block btn-common mb-4"><?=__('申请报名', 'young-bird')?></a>
-            <?php else: ?>
-            <button type="button" disabled class="btn btn-outline-primary mx-auto d-block btn-common mb-4 attend-activity"><?=__('已申请报名', 'young-bird')?></button>
-          <?php
-              endif;
-            else: ?>
-          <a href="<?=get_the_permalink($event_id)?>" class="btn btn-outline-primary mx-auto d-block btn-common mb-4"><?=__('立即报名', 'young-bird')?></a>
-          <?php
-            endif;
-          endif; ?>
-          <?php if (get_field('attendable', $id_dl)): if (in_array($id_dl, get_user_meta(get_current_user_id(), 'attend_activities') ?: array())): ?>
-          <button type="button" disabled class="btn btn-outline-primary mx-auto d-block btn-common mb-4 attend-activity"><?=__('已报名', 'young-bird')?></button>
-          <?php else: ?>
-          <a href="<?=get_permalink(get_page_by_path('user-center')->ID)?>?attend-activity=<?=$id_dl?>" class="btn btn-outline-primary mx-auto d-block btn-common mb-4 attend-activity"><?=__('立即报名', 'young-bird')?></a>
-          <?php endif; endif; ?>
-          <?php if (get_field('recruitment', $id_dl)): if (in_array($id_dl, get_user_meta(get_current_user_id(), 'apply_jobs') ?: array())): ?>
-          <button type="button" disabled class="btn btn-outline-primary mx-auto d-block btn-common mb-4 attend-activity"><?=__('已投递简历', 'young-bird')?></button>
-          <?php else: ?>
-          <a href="<?=get_permalink(get_page_by_path('user-center')->ID)?>?recruitment=<?=$id_dl?>" class="btn btn-outline-primary mx-auto d-block btn-common mb-4 apply-joy"><?=__('投递简历', 'young-bird')?></a>
-          <?php endif; endif; ?>
+          <?php include(locate_template('single-action-button.php')); ?>
         </div>
         <div class="col-md-6 news-detail-ad">
           <?php foreach (get_posts(array ('category_name' => 'news-detail-ad')) as $ad): ?>
