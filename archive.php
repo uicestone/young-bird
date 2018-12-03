7790090850af752a31fb0ddc3e93440ca937302c
Youngbirdplan <isaye@youngbird.com.cn>
2018-12-03 13:39:38 +0800

    修改ad显示条目为无限多(100个) 修改ad显示条目为无限多(100个)

--- archive.php
+++ archive.php
@@ -48,7 +48,7 @@ get_header(); ?>
           <?php endif; ?>
         </div>
         <div class="col-md-6">
-          <?php foreach (get_posts(array ('category_name' => 'news-list-ad')) as $ad): ?>
+          <?php foreach (get_posts(array ('category_name' => 'news-list-ad','posts_per_page' => 100)) as $ad): ?>
           <a href="<?=get_the_permalink($ad)?>" class="card mb-3 item-sub-history">
             <div>
               <img src="<?=get_field('ad_thumbnail', $ad->ID)['url']?>" class="card-img-top">
